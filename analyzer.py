d083d1a4d0a6b2f30c526f45416b393d42b358a4
yimin.chu <yimin.chu@smart-foa.com>
2019-04-02 18:26:52 +0900

    bug修正　今ip/userのデータを取れるようになった。続いて分析とグルーピングを行います。

--- analyzer.py
+++ analyzer.py
@@ -5,8 +5,10 @@ import datetime
 import re
 import time
 import ipqueryhtmlparser
+import ipinfo
 
 class analyzer :
+    mytoken = '7909e13247806a'
     mysqlhost = '118.27.35.79'
     mysqlusername = 'collindoyle'
     mysqlpw = 'Tatsumi1983'
@@ -16,14 +18,35 @@ class analyzer :
     logdict = dict()
     http = urllib3.PoolManager()
     parser = ipqueryhtmlparser.ipqueryparser()
+    def GetIpInfo(self, ip):
+        handler = ipinfo.getHandler(analyzer.mytoken)
+        details = handler.getDetails(ip)
+        mydb = mysql.connector.connect(host = analyzer.mysqlhost, user=analyzer.mysqlusername, passwd = analyzer.mysqlpw, database = 'chatlog')
+        mycursor = mydb.cursor()
+        sqlquery = "INSERT INTO ipInfo (ipaddress, postcode, Country, Prefecture, City, Provider) VALUES (%s, %s, %s, %s, %s, %s)"
+        if hasattr(details,'postal'):
+            postal = details.postal
+        else:
+            postal = '000-0000'
+        mycursor.execute(sqlquery, (details.ip, postal, details.country_name, details.region, details.city, details.org))
+        mydb.commit()
+        mycursor.close()
+        mydb.close()
+        return postal
+
     def GetPostCode(self, ip):
-        time.sleep(1)
-        queryurl = analyzer.ipquerytemplate + ip
-        response = analyzer.http.request('GET', queryurl)
-        resultbody = response.data.decode('utf8', errors = 'ignore')
-        analyzer.parser.feed(resultbody)
-        postcode = analyzer.parser.datalist[4]
-        print(postcode)
+        mydb = mysql.connector.connect(host = analyzer.mysqlhost, user = analyzer.mysqlusername, passwd = analyzer.mysqlpw, database = 'chatlog')
+        mycursor = mydb.cursor()
+        sqlquery = "SELECT * FROM ipInfo WHERE ipaddress = %s"
+        mycursor.execute(sqlquery, (ip,))
+        lst = mycursor.fetchall()
+        if len(lst) > 0:            
+            row = lst[0]
+            postcode = row[1]
+        else:
+            postcode = self.GetIpInfo(ip)
+        #mycursor.close()
+        mydb.close()
         return postcode
 
     def ProcessLogs(self):
@@ -52,13 +75,27 @@ class analyzer :
                     analyzer.logdict.pop(t)
                     entry = (r['name'],r['ip'],r['timestamp'],record['timestamp'])
                     entrylist.append(entry)
-            col.update_one({'_id': record['_id']},{'$set', {'processed': True}})
+                    col.update_one({'_id': r['_id']}, {'$set' : {'processed': True}})
+                    col.update_one({'_id': record['_id']}, {'$set': {'processed': True}})
+                else:
+                    entry = (record['name'], record['ip'], record['timestamp'], record['timestamp'])
+                    entrylist.append(entry)
+                    col.update_one({'_id': record['_id']}, {'$set': {'processed': True}})
+        for remainedrecord in list(analyzer.logdict.values()):
+            now = datetime.datetime.now()
+            if now - remainedrecord['timestamp'] > datetime.timedelta(days=1):
+                entry = (remainedrecord['name'], remainedrecord['ip'], remainedrecord['timestamp'], remainedrecord['timestamp'] + datetime.timedelta(hours = 1))
+                entrylist.append(entry)
+                col.update_one({'_id': remainedrecord['_id']}, {'$set': {'processed': True}})
+                analyzer.logdict.pop((remainedrecord['name'],remainedrecord['ip']))
         mydb = mysql.connector.connect(host = analyzer.mysqlhost, user = analyzer.mysqlusername, passwd = analyzer.mysqlpw, database = 'chatlog')
         mycursor = mydb.cursor()
         for entry in entrylist:
             ip = entry[1]
             postcode = self.GetPostCode(ip)
-            sqlquery = "INSERT INTO loginrecords (username, ipaddress, logintime, logouttime, postcode) VALUES (%s, %s, '%s', '%s', %s)"            
-            result = mycursor.execute(sqlquery, (entry[0], entry[1], entry[2], entry[3], postcode))
+            sqlquery = "INSERT INTO loginrecords (username, ipaddress, logintime, logouttime, postcode) VALUES (%s, %s, %s, %s, %s)"            
+            mycursor.execute(sqlquery, (entry[0], entry[1], entry[2], entry[3], postcode))
             mydb.commit()
+        mycursor.close()
+        mydb.close()
 
